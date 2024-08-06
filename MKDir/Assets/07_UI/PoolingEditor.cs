a45408fdcb362d2acf9cc3cc5d49d380db885471
Minkyo <minrim0615@gmail.com>
2024-08-07 00:51:37 +0900

    Upgrade UI

--- MKDir/Assets/07_UI/PoolingEditor.cs
+++ MKDir/Assets/07_UI/PoolingEditor.cs
@@ -2,32 +2,29 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace MKDir.ObjectPooling
+public class PoolingEditor : EditorWindow
 {
-    public class PoolingEditor : EditorWindow
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("MkDir/PoolingEditor")]
+    public static void ShowExample()
     {
-        [SerializeField]
-        private VisualTreeAsset m_VisualTreeAsset = default;
+        PoolingEditor wnd = GetWindow<PoolingEditor>();
+        wnd.titleContent = new GUIContent("PoolingEditor");
+    }
 
-        [MenuItem("Mkdir/PoolingEditor")]
-        public static void ShowExample()
-        {
-            PoolingEditor wnd = GetWindow<PoolingEditor>();
-            wnd.titleContent = new GUIContent("PoolingEditor");
-        }
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
 
-        public void CreateGUI()
-        {
-            // Each editor window contains a root VisualElement object
-            VisualElement root = this.rootVisualElement;
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
 
-            // VisualElements objects can contain other VisualElement following a tree hierarchy.
-            VisualElement label = new Label("Hello World! From C#");
-            root.Add(label);
-
-            // Instantiate UXML
-            VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-            root.Add(labelFromUXML);
-        }
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
     }
 }
--- MKDir/Assets/07_UI/PoolingItem/PoolingItem.cs
+++ MKDir/Assets/07_UI/PoolingItem/PoolingItem.cs
@@ -4,5 +4,9 @@ using UnityEngine.UIElements;
 public class PoolingItem : MonoBehaviour
 {
     //? 음...
+    //* 뭐해야 할까? 
+
+    //* 이거는 Create Item을 하면 생겨나야 해 그리고 이걸 누르면 오른쪽에 정보가 표시되야 해
+    //* 이걸 생성하면 SO도 같이 생성되서 정보를 기록해야 해 
     private VisualElement _root;
 }
